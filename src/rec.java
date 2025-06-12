2fe007dd688d4cfe1b109387258a46b329e2364b
Vaishnavi <vaishnavipatil.9333@gmail.com>
2025-06-12 18:11:22 +0530

    kkadd

--- src/rec.java
+++ src/rec.java
@@ -1,16 +1,16 @@
 public class rec {
-   public static void  dec(int n)
-   {
-       if(n==0)
-       {
-           return;
-       }
-       System.out.println(n);
-       dec(n-1);
-   }
-   public static void main(String args[])
-   {
-       int n=5;
-       dec(n);
-   }
+    public static void  dec(int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+    public static void main(String args[])
+    {
+        int n=5;
+        dec(n);
+    }
 }
