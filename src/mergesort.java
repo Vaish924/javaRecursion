1d37353fd1efdd62995a2e250474769531e5be98
Vaishnavi <vaishnavipatil.9333@gmail.com>
2025-06-15 19:49:46 +0530

    add

---
+++ src/mergesort.java
@@ -0,0 +1,55 @@
+public class mergesort{
+
+    public static void divide(int[] arr, int si,int ei)
+    {
+        if(si>=ei)
+        {
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        divide(arr,si,mid);
+        divide(arr,mid+1,ei);
+        conquer(arr,si,mid,ei);
+
+    }
+    public static void conquer(int[] arr, int si, int mid,int ei)
+    {
+        int newarr[]=new int[ei-si+1];
+        int idx1=si;
+        int idx2=mid+1;
+        int x=0;
+        while(idx1<=mid&&idx2<=ei)
+        {
+            if(arr[idx1]<=arr[idx2])
+            {
+                newarr[x++]=arr[idx1++];
+            }
+            else {
+                newarr[x++]=arr[idx2++];
+            }
+        }
+        while(idx1<=mid)
+        {
+            newarr[x++]=arr[idx1++];
+        }
+        while(idx2<=ei)
+        {
+            newarr[x++]=arr[idx2++];
+        }
+        for(int i=0;i<newarr.length;i++)
+        {
+            arr[si+i]=newarr[i];
+        }
+    }
+    public static void main(String args[])
+    {
+        int arr[]={4,2,9,5,3,7};
+        int n=arr.length;
+        divide(arr,0,n-1);
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(arr[i]+" ");
+        }
+
+    }
+}
